9a3dea3d68d5d0737e4063174be58486fb9f64c0
Hüseyin Umut Kardaş <kardasumt@gmail.com>
2025-11-03 23:19:18 +0300

    Initial Commit

---
+++ WhatsApp/WhatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppApp.swift
+//  WhatsApp
+//
+//  Created by Hüseyin Umut Kardaş on 3.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
